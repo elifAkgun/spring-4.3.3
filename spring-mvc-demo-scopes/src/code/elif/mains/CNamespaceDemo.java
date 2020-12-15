d40f35d385f2e6f395e3037c2c08a90ab7a6ca35
elif.akgun <potrak@gmail.com>
2020-12-16 00:57:11 +0300

    c namespace was added for bean initialization

---
+++ spring-mvc-demo-scopes/src/code/elif/mains/CNamespaceDemo.java
@@ -0,0 +1,18 @@
+package code.elif.mains;
+
+import code.elif.model.Organization;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CNamespaceDemo {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans-namespace-c.xml");
+
+        Organization organization = (Organization) applicationContext.getBean("myOrg");
+
+        System.out.println(organization);
+
+        ((ClassPathXmlApplicationContext)applicationContext).close();
+    }
+}
